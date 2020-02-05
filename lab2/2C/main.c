3f7400b2dc299d801d1c5872ad717219ab656d64
andrewhlu <alu@ucsb.edu>
2020-02-04 18:13:54 -0800

    Completed Part 3A

--- lab2/2C/main.c
+++ lab2/2C/main.c
@@ -21,11 +21,81 @@ void Input_Capture_Setup() {
 }
 
 void TIM4_IRQHandler(void) {
-	// TODO
+	if((TIM4 -> SR) & TIM_SR_CC1IF) {
+		// An edge triggered this interrupt
+		if((GPIOB -> IDR) & GPIO_IDR_ID6) {
+			// A rising edge triggered this interrupt, store the counter value into lastValue
+			lastValue = (TIM4 -> CCR1) & TIM_CCR1_CCR1;
+		}
+		else {
+			// A falling edge triggered this interrupt, store the counter value into currentValue
+			// and calculate the time interval
+			currentValue = (TIM4 -> CCR1) & TIM_CCR1_CCR1;
+			timeInterval = currentValue - lastValue;
+		}
+	}
+	else if((TIM4 -> SR) & TIM_SR_UIF) {
+		// A counter overflow triggered this interrupt
+		overflowCount++;
+	}
+
+	// Clear the update interrupt flag.
+	TIM4 -> SR &= ~TIM_SR_UIF;
 }
 
 void Trigger_Setup() {
-	// TODO
+	// Enable GPIO Clock for PB6.
+	RCC -> AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+
+	// Set PB6 to alternative function mode and select alternate function 2 (TIM4_CH1)
+	GPIOB -> MODER &= ~GPIO_MODER_MODE6_0;
+	GPIOB -> MODER |= GPIO_MODER_MODE6_1;
+	GPIOB -> AFR[0] &= ~GPIO_AFRL_AFSEL6_0;
+	GPIOB -> AFR[0] |= GPIO_AFRL_AFSEL6_1;
+	GPIOB -> AFR[0] &= ~GPIO_AFRL_AFSEL6_2;
+	GPIOB -> AFR[0] &= ~GPIO_AFRL_AFSEL6_3;
+
+	// Set PB6 to no pull-up, no pull-down.
+	GPIOB -> PUPDR &= ~GPIO_PUPDR_PUPD6;
+
+	// Enable timer 4 in RCC_APB1ENRx
+	RCC -> APB1ENR1 |= RCC_APB1ENR1_TIM4EN;
+
+	// Enable auto reload preload in the control register and set the auto reload 
+	// value to its maximum value. Set the prescaler.
+	TIM4 -> ARR |= TIM_ARR_ARR;
+	TIM4 -> PSC = (uint32_t) 15999;
+
+	// In the capture/compare mode register, set the input capture mode bits such that 
+	// the input capture is mapped to timer input 1.
+	TIM4 -> CCMR1 |= TIM_CCMR1_CC1S_0;
+	TIM4 -> CCMR1 &= ~TIM_CCMR1_CC1S_1;
+
+	// In the capture/compare enable register, set bits to capture both rising/falling
+	// edges and enable capturing.
+	TIM4 -> CCER |= TIM_CCER_CC1P;
+	TIM4 -> CCER |= TIM_CCER_CC1NP;
+	TIM4 -> CCER |= TIM_CCER_CC1E;
+
+	// In the DMA/Interrupt enable register, enable both interrupt and DMA requests. In
+	// addition, enable the update interrupt.
+	TIM4 -> DIER |= TIM_DIER_CC1IE;
+	TIM4 -> DIER |= TIM_DIER_CC1DE;
+	TIM4 -> DIER |= TIM_DIER_UIE;
+
+	// Enable update generation in the event generation register.
+	TIM4 -> EGR |= TIM_EGR_UG;
+
+	// Clear the update interrupt flag.
+	TIM4 -> SR &= ~TIM_SR_UIF;
+
+	// Set the direction of the counter and enable the counter in the control register.
+	TIM4 -> CR1 &= ~TIM_CR1_DIR;
+	TIM4 -> CR1 |= TIM_CR1_CEN;
+
+	// Enable the interrupt (TIM4_IRQn) in the NVIC and set its priority to 2.
+	NVIC_EnableIRQ(TIM4_IRQn);
+	NVIC_SetPriority(TIM4_IRQn, 2);
 }
 
 int main(void) {	
@@ -51,7 +121,7 @@ int main(void) {
 	char message[6];
 	while(1) {
 		// Code for Part C1 -- Comment out when demoing Part C2
-		// TODO
+		sprintf(message, "%.06d", timeInterval);
 		
 		// Code for Part C2 -- Comment out when demoing Part C1
 		// TODO
